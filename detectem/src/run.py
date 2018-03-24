994f144f0226181a736518c86c594a81995f46cc
tishmen <topuzov.work@gmail.com>
2018-03-24 22:04:18 +0100

    changed port and use gunicorn

--- detectem/src/run.py
+++ detectem/src/run.py
@@ -7,7 +7,7 @@ from detectem.ws import do_detection
 
 def main():
     bottle.debug(DEBUG)
-    run(host='0.0.0.0', port=5723)
+    run(host='0.0.0.0', port=8080, server='gunicorn')
 
 
 if __name__ == '__main__':
